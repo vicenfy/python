960f575270d28418126260c70521420b6c711464
Fan <srtrack@outlook.com>
2018-03-01 21:31:02 +0800

    day 2

--- def.py
+++ def.py
@@ -3,3 +3,16 @@ def add(a,b):
     print(c)
 
 add(1,2)
+
+APPLE = 100
+a = None
+
+def fun():
+    global a #改变a为全局变量
+    a = 20
+    return a+100
+
+print(APPLE)
+print('a past=', a)
+print(fun())
+print('a now=', a)
