0a381370bc5582e83533c0757c73818b89afa255
Fan <srtrack@outlook.com>
2018-03-01 20:05:47 +0800

    python3基础

---
+++ variable.py
@@ -0,0 +1,9 @@
+apple = 10
+print(apple)
+appleEgg = 3
+print(apple + appleEgg)
+a=1
+b=2
+c = a,b
+print(a,b)
+print(c)
---
+++ while.py
@@ -0,0 +1,6 @@
+condition =1
+while condition < 10:
+	print(condition)
+	condition = condition +1
+while True:
+	print("I'm True")
