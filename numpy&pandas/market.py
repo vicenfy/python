fe17532924e3e940495b17a5b0176bfa28758181
Fan <srtrack@outlook.com>
2018-03-13 12:16:32 +0800

    day 5

---
+++ numpy&pandas/market.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+a = [0.4,0.6]
+b = [[0.8,0.2],[0.1,0.9]]
+n = 2
+
+def markrt(a, b):
+    c = [0, 0]
+    for i in range(0, n):
+        for j in range(0, n):
+            c[i] += a[j]*b[j][i]
+
+    if round(c[0],4) != round(a[0],4):
+        count += 1
+        markrt(c, b)
+
+    else:
+        print(c)
+
+# markrt(a, b)
+
+j = np.array([[0.8,0.2],[0.1,0.9]]) #b
+i = np.array([0.4,0.6]) #a
+
+def market(a,b):
+    is_equal = True
+    while is_equal:
+        temp = a
+        a = temp.dot(b)
+        for k in range(len(a)):
+            if a[k] != temp[k]:
+                break
+            else:
+                is_equal = False
+        
+    for k in range(len(a)):
+        a[k] = round(a[k], 4)
+
+    print(a)
+                
+market(i, j)
--- numpy&pandas/np.py
+++ numpy&pandas/np.py
@@ -29,3 +29,32 @@ print(e)
 
 f = np.linspace(1, 10, 6).reshape((2,3))
 print(f)
+#基础计算
+g = np.array([10,20,30,40])
+h = g**2
+print(h<=400)
+#点乘
+i = np.array([[0.8,0.2],[0.1,0.9]])
+j = np.array([0.4,0.6])
+k = np.dot(j, i)
+print(j.dot(i))
+#随机数
+a = np.random.random((2,4))
+print(a)
+print(a.sum(axis=1)) #axis=1行 0列
+print(a.min(axis=0)) #每列的最小数
+print(a.max(axis=1)) #每行的最大数
+# print(a.mean()) #平均值
+#特殊
+a = np.arange(14,2,-1).reshape(3,4)
+print(a)
+print(a.argmax()) #最大索引
+print(a.argmin())
+print(a.mean()) #平均值
+print(np.median(a))#中位数
+print(a.cumsum()) #累加
+print(np.diff(a)) #累差
+print(a.nonzero()) #两个array，分别表示行和列的index
+print(np.sort(a)) #每行的排序
+print((a.T).dot(a)) #转置
+print(a.clip(5,9)) #小于5的数变为5，大于9的数变为9，其他不变
